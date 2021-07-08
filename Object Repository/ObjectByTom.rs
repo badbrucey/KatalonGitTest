0b12fa7bb1256286c3602deb11c387330bc1c0bb
bruce <bruce@10.0.0.101>
2021-07-07 20:39:20 -0700

    AddedByTom

---
+++ Object Repository/ObjectByTom.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ObjectByTom</name>
+   <tag></tag>
+   <elementGuidId>8fd18aa0-5601-40c6-8f28-325c362a1d17</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
